2763625ba91bb34307de7e0d4d34db2b0a2eb205
Jin Li <lijin110110@gmail.com>
2022-03-08 12:04:23 +0800

    Change SPANN Index type to base Index type

--- AnnService/inc/Core/VectorIndex.h
+++ AnnService/inc/Core/VectorIndex.h
@@ -146,6 +146,11 @@ public:
 
     void BuildMetaMapping(bool p_checkDeleted = true);
 
+    virtual ErrorCode Append(SizeType headID, int appendNum, std::string* appendPosting, SizeType oldVID = -1) = 0;
+
+    virtual void ProcessAsyncReassign(std::unique_ptr<std::string> vectorContain, SizeType VID, std::vector<SizeType>& newHeads, bool check,
+                              SizeType oldVID, std::function<void()> p_callback) = 0;
+
 private:
     ErrorCode LoadIndexConfig(Helper::IniReader& p_reader);
 
